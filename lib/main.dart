e098d7507e7d96fdaa5a2e0431274c65280a0d9f
Azeem-Dev <Azeemsharif5822@gmail.com>
2024-04-09 14:22:54 +0500

    added navigation and sample pages
    flutter run -d windows to run on windows

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flut/pages/first_Page.dart';
+import 'package:flut/pages/second_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,177 +18,182 @@ class MainApp extends StatelessWidget {
     // List names = ['Abdul', 'Azeem', 'Awais'];
 
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        // home: Scaffold(
-        //TODO: BACKGROUND COLOR OF SCREEN
+      debugShowCheckedModeBanner: false,
+      // home: Scaffold(
+      //TODO: BACKGROUND COLOR OF SCREEN
 
-        // backgroundColor: Colors.amber[400],
+      // backgroundColor: Colors.amber[400],
 
-        //TODO: TOP BAR
+      //TODO: TOP BAR
 
-        // appBar: AppBar(
-        //   title: Text('My App bar'),
-        //   backgroundColor: Colors.amber[500],
-        //   elevation: 6,
-        //   leading: Icon(Icons.menu),
-        //   actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
-        // ),
+      // appBar: AppBar(
+      //   title: Text('My App bar'),
+      //   backgroundColor: Colors.amber[500],
+      //   elevation: 6,
+      //   leading: Icon(Icons.menu),
+      //   actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
+      // ),
 
-        //TODO: BODY OF THE SCREEN
+      //TODO: BODY OF THE SCREEN
 
-        //TODO: SOME SIMPLE STUFF IN BODY
-        // Center(
-        //   child: Container(
-        //       height: 300,
-        //       width: 300,
-        //       // color: Colors.black12,
-        //       decoration: BoxDecoration(
-        //           color: Colors.black12,
-        //           borderRadius: BorderRadius.circular(20)),
-        //       padding: const EdgeInsets.all(25),
-        //       // padding:EdgeInsets.symmetric(vertical: 100,horizontal: 20) ,
-        //       child: Icon(
-        //         Icons.favorite,
-        //         color: Colors.red,
-        //         size: 100,
-        //       )
-        //       // const Text('Azeem',
-        //       //     style: TextStyle(
-        //       //         color: Colors.white,
-        //       //         fontSize: 28,
-        //       //         fontWeight: FontWeight.bold)),
-        //       ),
-        // )
+      //TODO: SOME SIMPLE STUFF IN BODY
+      // Center(
+      //   child: Container(
+      //       height: 300,
+      //       width: 300,
+      //       // color: Colors.black12,
+      //       decoration: BoxDecoration(
+      //           color: Colors.black12,
+      //           borderRadius: BorderRadius.circular(20)),
+      //       padding: const EdgeInsets.all(25),
+      //       // padding:EdgeInsets.symmetric(vertical: 100,horizontal: 20) ,
+      //       child: Icon(
+      //         Icons.favorite,
+      //         color: Colors.red,
+      //         size: 100,
+      //       )
+      //       // const Text('Azeem',
+      //       //     style: TextStyle(
+      //       //         color: Colors.white,
+      //       //         fontSize: 28,
+      //       //         fontWeight: FontWeight.bold)),
+      //       ),
+      // )
 
-        //TODO: COLUMNS IN BODY
+      //TODO: COLUMNS IN BODY
 
-        // body: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        // crossAxisAlignment: CrossAxisAlignment.start,
+      // body: Column(
+      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      // crossAxisAlignment: CrossAxisAlignment.start,
 
-        //    children: [
-        // Container(
-        //   height: 350,
-        //   color: Colors.amber[300],
-        // ),
-        // Container(
-        //   height: 350,
-        //   color: Colors.amber[200],
-        // ),
-        // Container(
-        //   height: 350,
-        //   color: Colors.amber[100],
-        // ),
-        // Expanded(
-        //     flex: 3,
-        //     child: Container(
-        //       color: Colors.amber[300],
-        //     )),
-        // Expanded(
-        //     child: Container(
-        //   color: Colors.amber[200],
-        // )),
-        // Expanded(
-        //     child: Container(
-        //   color: Colors.amber[100],
-        // )),
+      //    children: [
+      // Container(
+      //   height: 350,
+      //   color: Colors.amber[300],
+      // ),
+      // Container(
+      //   height: 350,
+      //   color: Colors.amber[200],
+      // ),
+      // Container(
+      //   height: 350,
+      //   color: Colors.amber[100],
+      // ),
+      // Expanded(
+      //     flex: 3,
+      //     child: Container(
+      //       color: Colors.amber[300],
+      //     )),
+      // Expanded(
+      //     child: Container(
+      //   color: Colors.amber[200],
+      // )),
+      // Expanded(
+      //     child: Container(
+      //   color: Colors.amber[100],
+      // )),
 
-        //TODO: SCROLLABLE LIST VIEW
+      //TODO: SCROLLABLE LIST VIEW
 
-        //     body: ListView(
-        //   //TODO:CHANGE LIST SCROLL DIRECTION
-        //   // scrollDirection: Axis.horizontal,
-        //   children: [
-        //     Container(
-        //       height: 350,
-        //       color: Colors.amber[300],
-        //     ),
-        //     Container(
-        //       height: 350,
-        //       color: Colors.amber[200],
-        //     ),
-        //     Container(
-        //       height: 350,
-        //       color: Colors.amber[100],
-        //     ),
-        //   ],
-        // )
+      //     body: ListView(
+      //   //TODO:CHANGE LIST SCROLL DIRECTION
+      //   // scrollDirection: Axis.horizontal,
+      //   children: [
+      //     Container(
+      //       height: 350,
+      //       color: Colors.amber[300],
+      //     ),
+      //     Container(
+      //       height: 350,
+      //       color: Colors.amber[200],
+      //     ),
+      //     Container(
+      //       height: 350,
+      //       color: Colors.amber[100],
+      //     ),
+      //   ],
+      // )
 
-        //TODO: DYNAMIC LIST
-        // body: SizedBox(
-        //     width: 200,
-        //     child: ListView.builder(
-        //         itemCount: names.length,
-        //         itemBuilder: (context, index) => Container(
-        //               height: 200,
-        //               width: 200,
-        //               padding: EdgeInsets.only(
-        //                   left: 20, right: 20, bottom: 0, top: 0),
-        //               decoration: BoxDecoration(
-        //                 borderRadius: BorderRadius.circular(100),
-        //                 color: Colors.lime[200],
-        //               ),
-        //               child: Row(
-        //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //                 children: [
-        //                   Text('Name: ' + names[index]),
-        //                   Text('Age: 20')
-        //                 ],
-        //               ),
-        //             )))
+      //TODO: DYNAMIC LIST
+      // body: SizedBox(
+      //     width: 200,
+      //     child: ListView.builder(
+      //         itemCount: names.length,
+      //         itemBuilder: (context, index) => Container(
+      //               height: 200,
+      //               width: 200,
+      //               padding: EdgeInsets.only(
+      //                   left: 20, right: 20, bottom: 0, top: 0),
+      //               decoration: BoxDecoration(
+      //                 borderRadius: BorderRadius.circular(100),
+      //                 color: Colors.lime[200],
+      //               ),
+      //               child: Row(
+      //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //                 children: [
+      //                   Text('Name: ' + names[index]),
+      //                   Text('Age: 20')
+      //                 ],
+      //               ),
+      //             )))
 
-        //TODO: GridView
-        //   body: GridView.builder(
-        //       itemCount: 64,
-        //       gridDelegate:
-        //           //how many in each row
-        //           SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 24),
-        //       itemBuilder: (context, index) => Container(
-        //             color: Colors.amber[600],
-        //             margin: EdgeInsets.all(2),
-        //           ))
+      //TODO: GridView
+      //   body: GridView.builder(
+      //       itemCount: 64,
+      //       gridDelegate:
+      //           //how many in each row
+      //           SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 24),
+      //       itemBuilder: (context, index) => Container(
+      //             color: Colors.amber[600],
+      //             margin: EdgeInsets.all(2),
+      //           ))
 
-        //TODO: STACK
-        //     body: Stack(
-        //       alignment: Alignment.bottomRight,
-        //   children: [
-        //     //Big box
-        //     Container(
-        //       height: 300,
-        //       width: 300,
-        //       color: Colors.blue[400],
-        //     ),
-        //     //medium box
-        //     Container(
-        //       height: 200,
-        //       width: 200,
-        //       color: Colors.blue[300],
-        //     ),
-        //     //small box
-        //     Container(
-        //       height: 100,
-        //       width: 100,
-        //       color: Colors.blue[200],
-        //     ),
-        //   ],
-        // )
+      //TODO: STACK
+      //     body: Stack(
+      //       alignment: Alignment.bottomRight,
+      //   children: [
+      //     //Big box
+      //     Container(
+      //       height: 300,
+      //       width: 300,
+      //       color: Colors.blue[400],
+      //     ),
+      //     //medium box
+      //     Container(
+      //       height: 200,
+      //       width: 200,
+      //       color: Colors.blue[300],
+      //     ),
+      //     //small box
+      //     Container(
+      //       height: 100,
+      //       width: 100,
+      //       color: Colors.blue[200],
+      //     ),
+      //   ],
+      // )
 
-        //TODO: GESTURE DETECTOR
-        //     body: Center(
-        //   child: GestureDetector(
-        //       onTap: widgetTapped,
-        //       child: Container(
-        //         height: 200,
-        //         width: 200,
-        //         color: Colors.blue[300],
-        //         child: Center(child: Text('Tap me!')),
-        //       )),
-        // )
+      //TODO: GESTURE DETECTOR
+      //     body: Center(
+      //   child: GestureDetector(
+      //       onTap: widgetTapped,
+      //       child: Container(
+      //         height: 200,
+      //         width: 200,
+      //         color: Colors.blue[300],
+      //         child: Center(child: Text('Tap me!')),
+      //       )),
+      // )
 
-        // ),
+      // ),
 
-        //TODO: NAVIGATOR
+      //TODO: NAVIGATOR
 
-        home: FirstPage());
+      home: FirstPage(),
+      routes: {
+        '/firstPage': (context) => FirstPage(),
+        '/secondpage': (context) => SecondPage()
+      },
+    );
   }
 }
--- lib/pages/first_Page.dart
+++ lib/pages/first_Page.dart
@@ -13,14 +13,46 @@ class FirstPage extends StatelessWidget {
     return Scaffold(
       appBar:
           AppBar(title: Text('1st Page'), backgroundColor: Colors.amber[500]),
-      body: Center(
-          child: ElevatedButton(
-        child: Text('Go to Second Page'),
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => SecondPage()));
-        },
-      )),
+      drawer: Drawer(
+        backgroundColor: Colors.amber[300],
+        width: 200,
+        child: Column(
+          children: [
+            DrawerHeader(
+                child: Icon(
+              Icons.favorite,
+              size: 48,
+            )),
+
+            // home page list tile
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('H O M E'),
+              onTap: () {
+                Navigator.pushNamed(context, '/firstPage');
+              },
+            ),
+
+            //settings page list tile
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('S E T T I N G S'),
+               onTap: () {
+                Navigator.pushNamed(context, '/secondpage');
+              },
+            ),
+          ],
+        ),
+      ),
+      // body: Center(
+      //     child: ElevatedButton(
+      //   child: Text('Go to Second Page'),
+      //   onPressed: () {
+      //     // Navigator.push(
+      //     //     context, MaterialPageRoute(builder: (context) => SecondPage()));
+      //     Navigator.pushNamed(context, '/secondpage');
+      //   },
+      // )),
     );
   }
 }
